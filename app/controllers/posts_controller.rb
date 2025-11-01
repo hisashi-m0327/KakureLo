1ba49cc035813fcc6a0e9105dfd6000fd1e235f5
HisashiMiyashita <225394446+hisashi-m0327@users.noreply.github.com>
2025-11-01 13:28:48 +0000

    マイページの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
-      redirect_to posts_path
+      redirect_to @post, notice: "投稿しました。"
     else
       render :new
     end
